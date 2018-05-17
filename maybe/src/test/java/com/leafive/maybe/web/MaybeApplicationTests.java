b7d5e2454c6307d993b5cf3a70dd71869132b972
18321 <18321@LAPTOP-JPHHBK1U>
2018-05-17 09:35:13 +0800

    微调

---
+++ maybe/src/test/java/com/leafive/maybe/web/MaybeApplicationTests.java
@@ -0,0 +1,17 @@
+package com.leafive.maybe.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MaybeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+
+}
