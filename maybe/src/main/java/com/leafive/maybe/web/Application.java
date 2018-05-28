a88da90d7d3528abccb7ca820b6731a20701bec1
18321 <18321@LAPTOP-JPHHBK1U>
2018-05-28 15:52:36 +0800

    配置修改

--- maybe/src/main/java/com/leafive/maybe/web/Application.java
+++ maybe/src/main/java/com/leafive/maybe/web/Application.java
@@ -4,7 +4,8 @@ package com.leafive.maybe.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+@EnableDiscoveryClient
 @SpringBootApplication
 public class Application {
 
