eda95414a369e1c2a67a3874d1afd500d38af48f
Xiaoyu <Xiaoyu@DESKTOP-S9UP2NT>
2018-07-30 14:41:25 +0800

    新增lombook插件依赖

--- maybe/src/main/java/com/leafive/maybe/web/controller/MyController.java
+++ maybe/src/main/java/com/leafive/maybe/web/controller/MyController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.leafive.maybe.web.mapper.UserMapper;
@@ -23,7 +24,7 @@ public class MyController {
 		return "hello world!";
 	}
 	
-	@RequestMapping(value="/list",produces = "application/json; charset=utf-8")
+	@RequestMapping(value="/list", method = RequestMethod.GET)
 	public List getUserInfo() {
 		List<User> all = userMapper.getAll();
 		return all;
--- maybe/src/main/java/com/leafive/maybe/web/model/User.java
+++ maybe/src/main/java/com/leafive/maybe/web/model/User.java
@@ -1,5 +1,8 @@
 package com.leafive.maybe.web.model;
 
+import lombok.Data;
+
+@Data
 public class User {
 	private Integer id;
 
@@ -9,46 +12,7 @@ public class User {
 	
 	private String address;
 
-	public User() {
-	}
 
-	public Integer getId() {
-		return id;
-	}
-
-	public void setId(Integer id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public Integer getAge() {
-		return age;
-	}
-
-	public void setAge(Integer age) {
-		this.age = age;
-	}
-
-	public String getAddress() {
-		return address;
-	}
-
-	public void setAddress(String address) {
-		this.address = address;
-	}
-
-	@Override
-	public String toString() {
-		return "User [id=" + id + ", name=" + name + ", age=" + age + ", address=" + address + "]";
-	}
-	
 	
 	
 }
